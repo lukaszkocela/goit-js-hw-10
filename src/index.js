ba768992f81d468c5f63126cf55d995c7f1b21ef
Łukasz Kocela <lukaszkocela@gmail.com>
2023-04-25 10:07:02 +0200

    1st ver

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+
+const searchEl = document.getElementById('search-box');
 
 const DEBOUNCE_DELAY = 300;
+
+searchEl.addEventListener(
+  'input',
+  debounce(name => {
+    name = searchEl.value;
+    fetchCountries(name);
+  }, DEBOUNCE_DELAY)
+);
